09b7e4fece50de033d3fe2639b2a5afd5e12d96e
Rogan Micallef <micallro@shernet.sheridancollege.ca>
2024-02-08 14:13:00 -0500

    Player is moving and animating

---
+++ Assets/Week 5/Scripts/Knight.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Knight : MonoBehaviour
+{
+    Vector2 destination;
+    Vector2 movement;
+    public float speed = 3.0f;
+    Rigidbody2D rb;
+    Animator animator;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    private void FixedUpdate()
+    {
+        movement = destination - (Vector2)transform.position;
+
+        if(movement.magnitude < 0.1)
+        {
+            movement = Vector2.zero;
+        }
+
+        rb.MovePosition(rb.position + movement.normalized * speed * Time.deltaTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            destination = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
+        animator.SetFloat("Movement", movement.magnitude);
+    }
+}
