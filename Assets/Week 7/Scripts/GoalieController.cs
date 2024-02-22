1566b12dccfcad69f027020730b78cef10b9ef94
Rogan Micallef <micallro@shernet.sheridancollege.ca>
2024-02-22 17:18:56 -0500

    Task 4a complete, goalie teleports really well

---
+++ Assets/Week 7/Scripts/GoalieController.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalieController : MonoBehaviour
+{
+    public Rigidbody2D goalieRB;
+    Vector2 direction;
+    float magnitude;
+    public float radius = 2.4f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(GameController.SelectedPlayer == null) return;
+
+        direction = (Vector2)transform.position - (Vector2)GameController.SelectedPlayer.transform.position;
+        magnitude = direction.magnitude;
+        direction.Normalize();
+    }
+
+    private void FixedUpdate()
+    {
+        if(magnitude / 2 < radius)
+        {
+            goalieRB.position = (Vector2)transform.position - direction * magnitude / 2;
+        } 
+        else
+        {
+            goalieRB.position = (Vector2)transform.position - direction * radius;
+        }
+    }
+}
